33154f269d4e2ca2ca4fa554ba532ca6ac9bc678
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-19 18:36:31 +0500

    👌 IMPROVE: added reminderList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/Reminder';
+
+const reminderListData: Reminder[] = [
+  { id: 1, title: 'Reminder 1' },
+  { id: 2, title: 'Reminder 2' },
+  { id: 3, title: 'Reminder 3' },
+  { id: 4, title: 'Reminder 4' },
+  { id: 5, title: 'Reminder 5' }
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <ReminderList items={reminderListData} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
