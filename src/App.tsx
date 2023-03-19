042f475f765632be445d564a18b2b9e93c13734c
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-19 18:51:57 +0500

    📦 NEW: added reminder service API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/Reminder';
-
-const reminderListData: Reminder[] = [
-  { id: 1, title: 'Reminder 1' },
-  { id: 2, title: 'Reminder 2' },
-  { id: 3, title: 'Reminder 3' },
-  { id: 4, title: 'Reminder 4' },
-  { id: 5, title: 'Reminder 5' }
-];
+import reminderService from './services/reminder';
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
   return (
     <div>
-      <ReminderList items={reminderListData} />
+      <ReminderList items={reminders} />
     </div>
   );
 }
